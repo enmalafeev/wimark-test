95097483eae250302e12daac759ef8c2cc980a4d
enmalafeev <malafeev.en@yandex.ru>
2020-02-11 23:51:25 +0300

    add logic for store

--- src/components/TableUsers.vue
+++ src/components/TableUsers.vue
@@ -1,18 +1,33 @@
 <template lang="pug">
-  div.table-users
+  div.container
+    pre {{getUsersData}}
     h1.table-users__header Table of users
     table.table
       thead
         tr
-          th(
+          th.col(
             v-for="header in this.tableHeader"
           ) {{header}}
 
 </template>
 
 <script>
+import { mapActions, mapState, mapGetters } from 'vuex';
+
 export default {
   name: 'table-users',
+  computed: {
+    ...mapGetters(['getUsersData']),
+    ...mapState({
+      dataUsers: (state) => state.data,
+    }),
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    ...mapActions(['fetchData']),
+  },
   data() {
     return {
       tableHeader: [
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
+const corsOrigin = 'http://cors-anywhere.herokuapp.com/';
+const serverURL = 'https://rssi.wmrk.tk/';
+
 export default new Vuex.Store({
   state: {
+    rowData: {},
   },
   mutations: {
+    fillUpData(state, response) {
+      state.rowData = response;
+    },
   },
   actions: {
+    fetchData({ commit }) {
+      return axios
+        .get(`${corsOrigin}${serverURL}`)
+        .then((response) => {
+          commit('fillUpData', response.data);
+          console.log('addSkill', response.data);
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+  getters: {
+    getUsersData: (state) => state.rowData,
   },
   modules: {
   },
